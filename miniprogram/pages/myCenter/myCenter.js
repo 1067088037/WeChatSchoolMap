7d5779bddb452ab477d5c8802c0d2df5f4c6c515
wydwydwyd <d1286360646@outlook.com>
2021-04-19 15:06:33 +0800

    把“我的”页面的ui实现

--- miniprogram/pages/myCenter/myCenter.js
+++ miniprogram/pages/myCenter/myCenter.js
@@ -5,14 +5,35 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    userInfo:{},
+    hasUserInfo:false,
+    canIUseGetUserProfile: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    canIUseGetUserProfile: false,
+    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName')
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    if (wx.getUserProfile) {
+      this.setData({
+        canIUseGetUserProfile: true
+      })
+    }
+  },
+  getUserProfile(){
+    wx.getUserProfile({
+      desc:"用于完善用户信息",
+      sucess:res=>{
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true,
+        })
+        
+      }
+    })
   },
 
   /**
