afb75ff8b23da4094072078ef7b2ce1f1a827181
Ann000hjt <Ann000hjt@163.com>
2021-05-06 18:24:56 +0800

    显示评论

--- miniprogram/pages/building/building.js
+++ miniprogram/pages/building/building.js
@@ -72,54 +72,39 @@ Page({
       })
     })
   },
-      /**
-       * tipsAreaTap
-       * @param e
-       * @returns void
-       * @todo 显示攻略区
-       */
-      tipsAreaTap(e) {
-      this.setData({
-        showTipsArea: true,
-        introArea: false,
-        // 测试数据tips,正常使用时应从数据库获取
-        tips: [{
-          src: "/images/tabBarIcon/design_selected.png"
-        }, {
-          src: "/images/tabBarIcon/design_selected.png"
-        }, {
-          src: "/images/tabBarIcon/design_selected.png"
-        }, {
-          src: "/images/tabBarIcon/design_selected.png"
-        }, {
-          src: "/images/tabBarIcon/design_selected.png"
-        }]
-      })
+  /**
+   * tipsAreaTap
+   * @param e
+   * @returns void
+   * @todo 显示攻略区
+   */
+  tipsAreaTap(e) {
+    this.setData({
+      showTipsArea: true,
+      introArea: false,
+      // 测试数据tips,正常使用时应从数据库获取
+      tips: [{
+        src: "/images/tabBarIcon/design_selected.png"
+      }, {
+        src: "/images/tabBarIcon/design_selected.png"
+      }, {
+        src: "/images/tabBarIcon/design_selected.png"
+      }, {
+        src: "/images/tabBarIcon/design_selected.png"
+      }, {
+        src: "/images/tabBarIcon/design_selected.png"
+      }]
+    })
 
-    }, // end function
+  }, // end function
 
-      /**
-       * 生命周期函数--监听页面加载
-       */
-      onLoad: function (options) {
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
 
-      },
+  },
 
-<<<<<<< HEAD
-      /**
-       * 生命周期函数--监听页面初次渲染完成
-       */
-      onReady: function () {
-        console.log(app.globalData.markerId)
-        if (app.globalData.markerId) {
-          this.setData({
-            markerId: app.globalData.markerId
-
-          })
-        }
-        console.log(app.globalData.markerId, app.globalData.desLatitude, app.globalData.desLongtitude)
-      },
-=======
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -145,46 +130,45 @@ Page({
       })
     })
   },
->>>>>>> dev
 
-      /**
-       * 生命周期函数--监听页面显示
-       */
-      onShow: function () {
-      },
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  },
 
-      /**
-       * 生命周期函数--监听页面隐藏
-       */
-      onHide: function () {
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
 
-      },
+  },
 
-      /**
-       * 生命周期函数--监听页面卸载
-       */
-      onUnload: function () {
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
 
-      },
+  },
 
-      /**
-       * 页面相关事件处理函数--监听用户下拉动作
-       */
-      onPullDownRefresh: function () {
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
 
-      },
+  },
 
-      /**
-       * 页面上拉触底事件的处理函数
-       */
-      onReachBottom: function () {
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
 
-      },
+  },
 
-      /**
-       * 用户点击右上角分享
-       */
-      onShareAppMessage: function () {
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
 
-      }
+  }
 })
