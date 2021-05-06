15f51e23a6b9f39410ef53faa522c8dc466508a0
Ann000hjt <Ann000hjt@163.com>
2021-05-06 18:17:04 +0800

    完成了把数据库里的评论内容显示出来的功能

--- miniprogram/pages/building/building.js
+++ miniprogram/pages/building/building.js
@@ -26,7 +26,7 @@ Page({
   },
   //发表评论
   sendComment() {
-    db.comment.addComment(1, Campus, {
+    db.comment.addComment("1", Campus, {
       reply: null,
       text: this.data.commentValue,
       image: null
@@ -54,99 +54,105 @@ Page({
     }
   },
   intoCommentClick(e) {
-    this.setData({
-      showTipsArea: false,
-      showComment: true
-    })
-  },
-  /**
-   * tipsAreaTap
-   * @param e
-   * @returns void
-   * @todo 显示攻略区
-   */
-  tipsAreaTap(e) {
-    this.setData({
-      showTipsArea: true,
-      introArea: false,
-      // 测试数据tips,正常使用时应从数据库获取
-      tips: [{
-        src: "/images/tabBarIcon/design_selected.png"
-      }, {
-        src: "/images/tabBarIcon/design_selected.png"
-      }, {
-        src: "/images/tabBarIcon/design_selected.png"
-      }, {
-        src: "/images/tabBarIcon/design_selected.png"
-      }, {
-        src: "/images/tabBarIcon/design_selected.png"
-      }]
-    })
-
-  }, // end function
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    console.log(app.globalData.markerId)
-    if (app.globalData.markerId) {
-      this.setData({
-        markerId: app.globalData.markerId
-
-      })
-    }
-    console.log(app.globalData.markerId, app.globalData.desLatitude, app.globalData.desLongtitude)
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
     db.comment.getAllComment('1').then(res => {
-      console.log(res)
+      //console.log(res[0].text)
+      this.setData({
+        showTipsArea: false,
+        showComment: true,
+        comments: [{
+          text: res[0].text
+        }, {
+          text: res[1].text
+        }
+        ]
+      })
     })
   },
+      /**
+       * tipsAreaTap
+       * @param e
+       * @returns void
+       * @todo 显示攻略区
+       */
+      tipsAreaTap(e) {
+      this.setData({
+        showTipsArea: true,
+        introArea: false,
+        // 测试数据tips,正常使用时应从数据库获取
+        tips: [{
+          src: "/images/tabBarIcon/design_selected.png"
+        }, {
+          src: "/images/tabBarIcon/design_selected.png"
+        }, {
+          src: "/images/tabBarIcon/design_selected.png"
+        }, {
+          src: "/images/tabBarIcon/design_selected.png"
+        }, {
+          src: "/images/tabBarIcon/design_selected.png"
+        }]
+      })
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+    }, // end function
 
-  },
+      /**
+       * 生命周期函数--监听页面加载
+       */
+      onLoad: function (options) {
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+      },
 
-  },
+      /**
+       * 生命周期函数--监听页面初次渲染完成
+       */
+      onReady: function () {
+        console.log(app.globalData.markerId)
+        if (app.globalData.markerId) {
+          this.setData({
+            markerId: app.globalData.markerId
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+          })
+        }
+        console.log(app.globalData.markerId, app.globalData.desLatitude, app.globalData.desLongtitude)
+      },
 
-  },
+      /**
+       * 生命周期函数--监听页面显示
+       */
+      onShow: function () {
+      },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+      /**
+       * 生命周期函数--监听页面隐藏
+       */
+      onHide: function () {
 
-  },
+      },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+      /**
+       * 生命周期函数--监听页面卸载
+       */
+      onUnload: function () {
 
-  }
+      },
+
+      /**
+       * 页面相关事件处理函数--监听用户下拉动作
+       */
+      onPullDownRefresh: function () {
+
+      },
+
+      /**
+       * 页面上拉触底事件的处理函数
+       */
+      onReachBottom: function () {
+
+      },
+
+      /**
+       * 用户点击右上角分享
+       */
+      onShareAppMessage: function () {
+
+      }
 })
