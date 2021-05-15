1643d2aca842a674117070dc1a1f34e151614b3f
Summer-lights <1067088037@qq.com>
2021-05-15 19:23:28 +0800

    增加建筑物缓存功能
    建筑物不会每次都从数据库载入，而是在数据库有变的时候重新下载

--- miniprogram/util/database/arch.js
+++ miniprogram/util/database/arch.js
@@ -10,13 +10,46 @@ export class Arch {
    * @returns {Array} 建筑物数组 
    */
   async getArchArray(campusId) {
-    return await wx.cloud.callFunction({
-      name: 'getAllBySuperId',
+    let serverTime = new Date(getApp().globalData.campus.archLastUpdateTime).getTime()
+    let archStorage = wx.getStorageSync(campusId)
+    if (archStorage.lastUpdateTime >= serverTime) {
+      return new Promise((resolve, reject) => {
+        console.log('建筑物从本地缓存载入')
+        resolve(archStorage.archs)
+      })
+    } else {
+      console.log('本地建筑物数据不是最新的，从服务器载入建筑物信息')
+      return await wx.cloud.callFunction({
+        name: 'getAllBySuperId',
+        data: {
+          collection: 'arch',
+          superId: campusId
+        }
+      }).then(res => {
+        wx.setStorage({
+          key: campusId,
+          data: {
+            desc: '校区的建筑物缓存',
+            lastUpdateTime: new Date().getTime(),
+            archs: res.result.data
+          }
+        })
+        return res.result.data
+      }).catch(err => [])
+    }
+  }
+
+  /**
+   * 刷新最后一次更新建筑物的世界
+   * @param {string} campusId 校区ID
+   */
+  async refreshArchLastUpdateTime(campusId) {
+    console.log(campusId)
+    return _db.collection('campus').doc(campusId).update({
       data: {
-        collection: 'arch',
-        superId: campusId
+        archLastUpdateTime: new Date()
       }
-    }).then(res => res.result.data).catch(err => [])
+    })
   }
 
   /**
@@ -28,6 +61,7 @@ export class Arch {
     if (arch.constructor != Object) {
       console.error('arch类型非法')
     } else {
+      await this.refreshArchLastUpdateTime(campusId)
       return _db.collection('arch').add({
         data: {
           super: {
@@ -53,7 +87,9 @@ export class Arch {
     if (arch.constructor != Object) {
       console.error('arch类型非法')
     } else {
-      // console.log(arch)
+      _db.collection('arch').doc(archId).get().then(res => {
+        this.refreshArchLastUpdateTime(res.data.super._id)
+      })
       return _db.collection('arch').doc(archId).update({
         data: arch
       })
@@ -69,6 +105,11 @@ export class Arch {
     if (arch.constructor != Object) {
       console.error('arch类型非法')
     } else {
+      _db.collection('arch').where({
+        markId: markId
+      }).get().then(res => {
+        this.refreshArchLastUpdateTime(res.data.super._id)
+      })
       return _db.collection('arch').where({
         markId: markId
       }).update({
@@ -82,6 +123,9 @@ export class Arch {
    * @param {string} archId 建筑物ID
    */
   async removeArchById(archId) {
+    _db.collection('arch').doc(archId).get().then(res => {
+      this.refreshArchLastUpdateTime(res.data.super._id)
+    })
     await _db.collection('arch').doc(archId).remove()
     await _db.collection('strategy').where({ 'super._id': archId }).remove()
     return db.comment.removeAllComment(archId)
@@ -92,6 +136,11 @@ export class Arch {
    * @param {string} markId 建筑物标注ID
    */
   async removeArchByMarkId(markId) {
+    _db.collection('arch').where({
+      markId: markId
+    }).get().then(res => {
+      this.refreshArchLastUpdateTime(res.data.super._id)
+    })
     return await _db.collection('arch').where({
       markId: markId
     }).get().then(async res => {
