16b727f58a038588e6808842d0773fcf8fa2d0ba
Summer-lights <1067088037@qq.com>
2021-05-28 22:27:30 +0800

    修改名称

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -10,7 +10,7 @@ App({
     campus: null,      //校区信息
     buildingSelected: null, // 选中建筑对象
     archItem: [],
-    versionCode: 53
+    versionCode: 54
   },
   onLaunch: function () {
     if (!wx.cloud) {
