80e5ad69c84fa4eae12f15c1da76a3691820472a
Summer-lights <1067088037@qq.com>
2021-05-30 00:27:32 +0800

    尝试实时获取更新

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -10,9 +10,47 @@ App({
     campus: null,      //校区信息
     buildingSelected: null, // 选中建筑对象
     archItem: [],
-    versionCode: 62
+    versionCode: 64
   },
   onLaunch: function () {
+    console.log('当前版本:', this.globalData.versionCode)
+    
+    wx.showLoading({
+      title: '加载中..',
+      mask: true
+    })
+    //微信小程序用户及时更新到最新版本
+    const updateManager = wx.getUpdateManager()
+    updateManager.onCheckForUpdate(function (res) {
+      // 请求完新版本信息的回调
+      console.log('是否有新版本: ', + res.hasUpdate)
+      if (!res.hasUpdate) {
+        wx.hideLoading()
+      }
+    })
+    updateManager.onUpdateReady(function () {
+      wx.hideLoading()
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        showCancel: false,
+        success: function (res) {
+          if (res.confirm) {
+            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate()
+          }
+        }
+      })
+    })
+    updateManager.onUpdateFailed(function () {
+      wx.hideLoading();
+      wx.showModal({
+        title: '提示',
+        content: '检查到有新版本，但下载失败，请检查网络后重试',
+        showCancel: false
+      })
+    })
+
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
