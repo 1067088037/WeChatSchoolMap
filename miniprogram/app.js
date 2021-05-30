14365c1b70131fe3d67778cf4f767829c27f074e
Summer-lights <1067088037@qq.com>
2021-05-30 11:46:02 +0800

    修复loading显示与隐藏不配对的bug

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -14,22 +14,13 @@ App({
   },
   onLaunch: function () {
     console.log('当前版本:', this.globalData.versionCode)
-        
-    wx.showLoading({
-      title: '加载中..',
-      mask: true
-    })
     //微信小程序用户及时更新到最新版本
     const updateManager = wx.getUpdateManager()
     updateManager.onCheckForUpdate(function (res) {
       // 请求完新版本信息的回调
       console.log('是否有新版本: ', + res.hasUpdate)
-      if (!res.hasUpdate) {
-        wx.hideLoading()
-      }
     })
     updateManager.onUpdateReady(function () {
-      wx.hideLoading()
       wx.showModal({
         title: '更新提示',
         content: '新版本已经准备好，是否重启应用？',
@@ -43,7 +34,6 @@ App({
       })
     })
     updateManager.onUpdateFailed(function () {
-      wx.hideLoading();
       wx.showModal({
         title: '提示',
         content: '检查到有新版本，但下载失败，请检查网络后重试',
